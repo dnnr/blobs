7c2252d13f331b55193043659245ce755d5bbdb8
Daniel Danner <daniel@danner.de>
2017-01-03 17:35:45 +0100

    Add helper script to update gh-pages branch

---
+++ publish.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+set -x
+
+python3.4 generate.py
+
+tmpdir=$(mktemp -d)
+trap "rm -rf $tmpdir" EXIT
+git -C "$tmpdir" init
+
+cp -r static/ "$tmpdir"
+cp blobs.html "$tmpdir"/index.html
+
+(
+    cd "$tmpdir"
+    git add -A
+    git commit -m "…"
+    git push -f git@github.com:dnnr/blobs.git HEAD:gh-pages
+)
